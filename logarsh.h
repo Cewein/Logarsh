81ca9b6bce99c1f407ac993156592a4f4ecb3275
Cewein <pro.mnowak@gmail.com>
2019-08-19 11:22:28 +0200

    better log file
    time is put first, next is importance of the log, name of the log and description of the log
    
    `<Mon Aug 19 10:48:56 2019> [DEFAULT][test] ceci est une info`

--- logarsh.h
+++ logarsh.h
@@ -60,7 +60,7 @@ private:
 	std::fstream file;
 	Log() {
 		file.open("log.txt", std::fstream::out | std::fstream::trunc);
-		file << "Grab some milk and cookie, let's start the logs\n";
+		file << "Grab some milk and cookie, let's start the logs\n\n";
 #if LOGINIT == 1
 		inInit = false;
 #endif
@@ -72,8 +72,10 @@ private:
 	std::string time() {
 
 		std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		std::string str = std::ctime(&time);
+		std::string str = "<";
+		str += std::ctime(&time);
 		str.erase(str.size() - 1);
+		str += "> ";
 		return str;
 	}
 
@@ -89,14 +91,14 @@ public:
 #if LOGINIT == 1
 		if (inInit)
 		{
-			crash("Last init did not finish");
+			crash("init","Last init did not finish");
 			exit(1);
 		}
 
 		inInit = true;
 		start = std::chrono::system_clock::now();
 		initName = name;
-		data = "\n[LOADING] - " + time() + " - " + initName + " is loading";
+		data = time() + "[LOADING][" + initName + "] start loading\n";
 		write();
 #endif
 	};
@@ -104,7 +106,7 @@ public:
 	void initLog(std::string str)
 	{
 #if LOGINIT == 1
-		data = "\n\t  - " + str;
+		data = time() + "\t  - " + str + "\n";
 		write();
 #endif
 	};
@@ -115,40 +117,40 @@ public:
 		inInit = false;
 		end = std::chrono::system_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - start;
-		data = "\n[LOADING] - " + time() + " - " + initName + " succesfully loaded in " + std::to_string(elapsed_seconds.count()) + "s";
+		data = time() + "[LOADING][" + initName + "] succesfully loaded in " + std::to_string(elapsed_seconds.count()) + "s\n";
 		write();
 #endif
 	}
 
 
 	//Write a error in log file, this is a high prority log level
-	void error(std::string str) {
+	void error(std::string name, std::string str) {
 #ifdef LOG_ERROR
-		data = "\n[ ERROR ] - " + time() + " - " + str;
+		data = time() + "[ ERROR ][" + name + "] " + str + "\n";
 		write();
 #endif
 	};
 
 	//write warning in log file, this is a mid prority log level
-	void warning(std::string str) {
+	void warning(std::string name, std::string str) {
 #ifdef LOG_WARNING
-		data = "\n[WARNING] - " + time() + " - " + str;
+		data = time() + "[WARNING][" + name + "] " + str + "\n";
 		write();
 #endif
 	};
 	
 	//write info in log file, this is the lowest prority log level
-	void info(std::string str) {
+	void info(std::string name, std::string str) {
 #ifdef LOG_INFO
-		data = "\n[DEFAULT] - " + time() + " - " + str;
+		data = time() + "[DEFAULT][" + name + "] " + str + "\n";
 		write();
 #endif // LOG_INFO
 	};
 
 	//Write program crashes in log file, this one cannot be removed
-	void crash(std::string str)
+	void crash(std::string name, std::string str)
 	{
-		data = "\n[ CRASH ] - " + time() + " - " + str;
+		data = time() + "[ CRASH ][" + name + "] " + str + "\n";
 		write();
 	};
 
